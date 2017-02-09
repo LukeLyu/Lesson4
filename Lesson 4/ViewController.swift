2abae79ce17f86907ce38dca460a32e024d94286
Ke Lu <ke.lu@EF.com>
2017-02-09 19:07:11 +0800

    I want to save this

--- Lesson 4/ViewController.swift
+++ Lesson 4/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myLabel = UILabel(frame:CGRect(origin: .zero, size: CGSize(width: 150, height: 30)))
+        myLabel.text = "added"
+        view.addSubview(myLabel)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
 
